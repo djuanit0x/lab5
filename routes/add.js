f27055626bed95b4997c2093e7029736740901f6
djuanit0x <dennisjuanito@gmail.com>
2020-02-06 17:30:55 -0800

    Done

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,11 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function(request, response) {
+    // Your code goes here
+    const name = request.query.name;
+    const info = request.query.description;
+    const newFriends = {name: name, info: info, image: "http://lorempixel.com/400/400/people"};
+
+    data.friends.push(newFriends);
+    response.render("index", data);
+};
